4651f9cccb7d724530e3a1b195dafe6fa1d6cc96
Disha <dishay166@gmail.com>
2023-08-18 17:58:50 +0530

    chore:add logo

--- src/App.js
+++ src/App.js
@@ -47,7 +47,7 @@ function App() {
     error,
   };
   return (
-    <div className="bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% h-screen pt-36 px-2">
+    <div className="bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 h-screen pt-36 px-2">
       <div className="container mx-auto max-w-4xl bg-white rounded-md shadow">
         <div className="md:grid md:grid-cols-3">
           <InputContext.Provider value={value}>
